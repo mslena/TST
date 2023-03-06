1215f5dfb270951a2658d124f6f7e7311eb39c8e
LAPTOP-HG83B24E\Dr Diana <ms.lena58@icloud.com>
2023-03-06 15:03:20 +0300

    NO

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            if (45 > 23)
+            {
+                Console.WriteLine("NO!!!!!!");
+            }
         }
     }
 }
